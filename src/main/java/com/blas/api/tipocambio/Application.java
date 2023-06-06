d0da40373ce1a50c5dd0100b040201a2ae176287
YATACOBLAS <juan_blas_1905@hotmail.com>
2023-06-05 20:48:14 -0500

    add first commit project

---
+++ src/main/java/com/blas/api/tipocambio/Application.java
@@ -0,0 +1,13 @@
+package com.blas.api.tipocambio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
